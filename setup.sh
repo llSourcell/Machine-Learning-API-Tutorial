f1e0164219c40974c38cbf5fc61f993d5934831f
Siraj Raval <sirajravel@gmail.com>
2018-07-20 15:52:45 -0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,4 @@
+cd /output
+curl -O http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
+curl -O http://msvocds.blob.core.windows.net/coco2014/train2014.zip
+unzip train2014.zip
---
+++ src/utils.py
@@ -0,0 +1,43 @@
+import scipy.misc, numpy as np, os, sys
+
+def save_img(out_path, img):
+    img = np.clip(img, 0, 255).astype(np.uint8)
+    scipy.misc.imsave(out_path, img)
+
+def scale_img(style_path, style_scale):
+    scale = float(style_scale)
+    o0, o1, o2 = scipy.misc.imread(style_path, mode='RGB').shape
+    scale = float(style_scale)
+    new_shape = (int(o0 * scale), int(o1 * scale), o2)
+    style_target = _get_img(style_path, img_size=new_shape)
+    return style_target
+
+def get_img(src, img_size=False):
+   img = scipy.misc.imread(src, mode='RGB') # misc.imresize(, (256, 256, 3))
+   if not (len(img.shape) == 3 and img.shape[2] == 3):
+       img = np.dstack((img,img,img))
+   if img_size != False:
+       img = scipy.misc.imresize(img, img_size)
+   return img
+
+def exists(p, msg):
+    assert os.path.exists(p), msg
+
+def list_files(in_path):
+    files = []
+    for (dirpath, dirnames, filenames) in os.walk(in_path):
+        files.extend(filenames)
+        break
+
+    return files
+
+def check_version():
+    if sys.version_info[0] != 2:
+        err_str = (
+            "This project only supports Python 2! Either run using "
+            "Python 2 or submit a pull request to "
+            "https://github.com/lengstrom/fast-style-transfer/ "
+            "to make the project version neutral!"
+        )
+
+        raise Exception(err_str)
